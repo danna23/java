85b087883464acff733ac9aec341ac3e9cf9fc07
danna23 <47341558+danna23@users.noreply.github.com>
2019-02-28 15:05:14 -0600

    Add files via upload

---
+++ ejer/Ejercicio 2/NUMERO/src/numero/NUMERO.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package numero;
+
+/**
+ *
+ * @author daann
+ */
+public class NUMERO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String[] letras = new String[] {"A","B","C","D","E","F","G","H","I","J","K","L","N","O","P","Q","R","S","T","U","V","W","Z"};
+        int aguardador = 0;
+        
+        System.out.println("Ingrese un numero de 0 a 24");
+        
+    }
+    
+}
